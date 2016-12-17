30862c0f6251ebf64676d6ff181e925aa5cd5685
JiminWen <jiminwen@tamu.edu>
2016-12-17 02:51:30 -0600

    third maximum value

---
+++ List/148. Sort List.java
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode sortList(ListNode head) {
+    if(head==null){
+        return null;
+    }
+     
+    
+        
+    }
+    private 
+}
